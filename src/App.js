237846bb98b439cc45d23058181291cacdbf42ca
bishalmallick <bishalmallick@gmail.com>
2021-05-25 01:49:52 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+/* eslint no-eval: 0 */
+import { useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+const [expression, setExpression] = useState("");
+const [answer, setAnswer] = useState(0);
+
+const display = (symbol) => {
+  setExpression(prev => prev + symbol);
+  if(expression[expression.length-1] === "="){
+    if(/[0-9.]/.test(symbol)) {
+      setExpression(symbol);
+    }
+    else {
+      setExpression(answer + symbol);
+    }
+  }
+};
+
+const calculate = () => {
+  if(expression.length > 0) {
+  setAnswer(eval(expression));
+  }
+  else {
+    setAnswer("0");
+  }
+}
+
+const allClear = () => {
+  setExpression("");
+  setAnswer(0);
+}
+const clear = () => {
+  setExpression(prev => prev.split("").slice(0, prev.length-1).join(""));
+  setAnswer(0);
+}
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">Javascript Calculator</div>
+      <div className="grid">
+        <div className="dis">
+          <input type="text" value={expression} placeholder="0" disabled/>
+          <div className="total">{answer}</div>
+        </div>
+        <div onClick={allClear} className="padButton AC tomato">AC</div>
+        <div onClick={clear} className="padButton C tomato">C</div>
+        <div onClick={() => display("/")} className="padButton div">/</div>
+        <div onClick={() => display("*")} className="padButton mult">X</div>
+        <div onClick={() => display("7")} className="padButton seven dark-gray">7</div>
+        <div onClick={() => display("8")} className="padButton eight dark-gray">8</div>
+        <div onClick={() => display("9")} className="padButton nine dark-gray">9</div>
+        <div onClick={() => display("-")} className="padButton minus">-</div>
+        <div onClick={() => display("4")} className="padButton four dark-gray">4</div>
+        <div onClick={() => display("5")} className="padButton five dark-gray">5</div>
+        <div onClick={() => display("6")} className="padButton six dark-gray">6</div>
+        <div onClick={() => display("+")} className="padButton plus">+</div>
+        <div onClick={() => display("1")} className="padButton one dark-gray">1</div>
+        <div onClick={() => display("2")} className="padButton two dark-gray">2</div>
+        <div onClick={() => display("3")} className="padButton three dark-gray">3</div>
+        <div onClick={calculate} className="padButton equal">=</div>
+        <div onClick={() => display("0")} className="padButton zero dark-gray">0</div>
+        <div onClick={() => display(".")} className="padButton dot">.</div>
+      </div>
+      <div className="footer">
+        <footer>
+          <p className="footer__text">
+          © 2021 - Website developed by <a href="https://github.com/bishalmallick" target="_blank" rel="noopener noreferrer"> Bishal Mallick</a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
